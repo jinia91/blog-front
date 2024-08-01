33279978bcc08ef2660efb084fd07178ece2dbd3
jinia91 <creee77@gmail.com>
2024-08-01 11:40:34 +0900

    polish

--- generate-coverage-report.js
+++ generate-coverage-report.js
@@ -3,6 +3,18 @@ const fs = require('fs');
 const coverageSummary = JSON.parse(fs.readFileSync('./coverage/coverage-summary.json', 'utf8'));
 
 let markdownReport = `
+## Summary
+
+| Metric      | Percentage |
+| ----------- | ---------- |
+| Statements  | ${coverageSummary.total.statements.pct}% |
+| Branches    | ${coverageSummary.total.branches.pct}% |
+| Functions   | ${coverageSummary.total.functions.pct}% |
+| Lines       | ${coverageSummary.total.lines.pct}% |
+\n`;
+
+
+markdownReport += `
 ## Test Coverage Report
 
 | File        | Statements | Branches | Functions | Lines |
@@ -14,16 +26,5 @@ for (const [file, metrics] of Object.entries(coverageSummary)) {
     markdownReport += `| ${file.replace('home/runner/work/blog-front/blog-front/app/', '')} | ${metrics.statements.pct}% | ${metrics.branches.pct}% | ${metrics.functions.pct}% | ${metrics.lines.pct}% |\n`;
   }
 }
-
-markdownReport += `
-## Summary
-
-| Metric      | Percentage |
-| ----------- | ---------- |
-| Statements  | ${coverageSummary.total.statements.pct}% |
-| Branches    | ${coverageSummary.total.branches.pct}% |
-| Functions   | ${coverageSummary.total.functions.pct}% |
-| Lines       | ${coverageSummary.total.lines.pct}% |
-`;
 
 fs.writeFileSync('coverage-report.md', markdownReport);
