572a824d575a96ddb567fda3d3af435cfc3ee31a
jinia91 <creee77@gmail.com>
2024-08-18 13:42:45 +0900

    env host 수정

--- app/(utils)/constants.ts
+++ app/(utils)/constants.ts
@@ -1,3 +1,3 @@
-export const HOST = process.env.HOST + 'api'
+export const HOST = 'http://' + process.env.HOST + '/api'
 export const LOGIN_PATH = '/login/oauth2'
 export const EMPTY_PATH = '/empty'
