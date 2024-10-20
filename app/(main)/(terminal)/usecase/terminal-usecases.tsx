04328a75e87fe3cf837f0424d97645840b01da0c
jinia91 <creee77@gmail.com>
2024-10-20 16:07:13 +0900

    히스토리 커맨드

--- app/(main)/(terminal)/usecase/terminal-usecases.tsx
+++ app/(main)/(terminal)/usecase/terminal-usecases.tsx
@@ -3,10 +3,11 @@ import type { TerminalContext } from '../domain/terminal-context'
 import { logo, welcomeCommand } from './welcome-command'
 import { clearCommand } from './clear-command'
 import { helpCommand } from './help-command'
+import { historyCommand } from './history-command'
 
 export const terminalUsecases = atom<TerminalContext>({
   history: [],
   output: [logo]
 })
 
-export const COMMAND_LIST = [clearCommand, welcomeCommand, helpCommand]
+export const COMMAND_LIST = [clearCommand, welcomeCommand, helpCommand, historyCommand]
