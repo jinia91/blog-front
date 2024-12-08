b8fd92a681840597123947fba399f0a98d0f5e50
jinia91 <creee77@gmail.com>
2024-12-08 16:28:13 +0900

    default carousel skeleton

---
+++ app/blog/(components)/carousel.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+export default function SwiperCarousel (): React.ReactElement {
+  return (
+    <div className="max-w-4xl mx-auto mt-8">
+      <h1 className="text-center text-2xl font-bold mb-4">Swiper Carousel</h1>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={50}
+        slidesPerView={1}
+      >
+        <SwiperSlide>
+          <div className="h-64 bg-blue-500 flex items-center justify-center text-white text-xl">
+            Slide 1
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-64 bg-red-500 flex items-center justify-center text-white text-xl">
+            Slide 2
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-64 bg-green-500 flex items-center justify-center text-white text-xl">
+            Slide 3
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="h-64 bg-yellow-500 flex items-center justify-center text-white text-xl">
+            Slide 4
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SwiperCarousel from './(components)/carousel'
+
+export default function blog (): React.ReactElement {
+  return (
+    <main className="">
+      <p>blog 페이지입니다</p>
+      <p>blog 페이지입니다</p>
+      <p>blog 페이지입니다</p>
+      <p>blog 페이지입니다</p>
+      <p>blog 페이지입니다</p>
+      <SwiperCarousel/>
+    </main>
+  )
+}
