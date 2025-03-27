191961eead3b954a92d5f87cb21362662c4b99bc
Jinia <creee77@gmail.com>
2025-03-27 14:31:33 +0900

    블로그 게시글 검색기능 (#44)
    * 1차 구현 완료 , 렌더링 테스트 필요
    
    * 검색기능

--- app/blog/(infra)/article.ts
+++ app/blog/(infra)/article.ts
@@ -2,10 +2,12 @@ import { type Article } from '../(domain)/article'
 import { HOST } from '../../(utils)/constants'
 import { withAuthRetry } from '../../login/(infra)/auth-api'
 
-export async function fetchArticlesByOffset (cursor: number, limit: number, isPublish: boolean): Promise<Article[]> {
+export async function fetchArticlesByOffset (cursor: number | null, limit: number, isPublish: boolean): Promise<Article[]> {
   const status = isPublish ? 'PUBLISHED' : 'DRAFT'
+  const refinedCursor = cursor ?? Number.MAX_SAFE_INTEGER - 1
+  console.log('요청 확인', HOST + '/v1/articles/simple?cursor=' + refinedCursor + '&limit=' + limit + '&status=' + status)
   const apiCall = async (): Promise<Response> => {
-    return await fetch(HOST + '/v1/articles/simple?cursor=' + cursor + '&limit=' + limit + '&status=' + status, {
+    return await fetch(HOST + '/v1/articles/simple?cursor=' + refinedCursor + '&limit=' + limit + '&status=' + status, {
       method: 'GET',
       credentials: 'include',
       cache: 'no-cache'
@@ -16,6 +18,7 @@ export async function fetchArticlesByOffset (cursor: number, limit: number, isPu
     console.error('아티클 조회 실패')
     return []
   }
+
   const data = await response.json()
   return data.map((article: any) => {
     return {
@@ -105,4 +108,34 @@ export async function initDraftArticle (): Promise<string | null> {
   }
   const data = await response.json()
   return data.articleId
+}
+
+export async function searchArticleByKeyword (keyword: string): Promise<Article[]> {
+  const apiCall = async (): Promise<Response> => {
+    return await fetch(HOST + '/v1/articles/simple?status=PUBLISHED&keyword=' + keyword, {
+      method: 'GET',
+      credentials: 'include',
+      cache: 'no-cache'
+    })
+  }
+  const response = await withAuthRetry(apiCall)
+  if (!response.ok) {
+    console.error('아티클 조회 실패')
+    return []
+  }
+  const data = await response.json()
+  return data.map((article: any) => {
+    return {
+      id: article.id,
+      title: article.title,
+      content: article.content,
+      thumbnail: article.thumbnailUrl,
+      tags: Array.isArray(article.tags) ? article.tags : [],
+      likes: 1,
+      comments: 1,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      createdAt: new Date(article.createdAt),
+      isPublished: true
+    }
+  })
 }
