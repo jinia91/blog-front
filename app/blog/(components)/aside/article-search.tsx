597dae14325bdf1b28f13a7a3cbbe56060959f35
jinia91 <creee77@gmail.com>
2025-04-09 03:39:04 +0900

    검색 수정

--- app/blog/(components)/aside/article-search.tsx
+++ app/blog/(components)/aside/article-search.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FiSearch } from 'react-icons/fi'
 import { useRouter, useSearchParams } from 'next/navigation'
 
@@ -8,8 +8,10 @@ export const ArticleSearchInput = (): React.ReactElement => {
   const searchParams = useSearchParams()
   const keyword = searchParams.get('keyword') ?? ''
 
+  const [inputValue, setInputValue] = React.useState(keyword)
+
   const inputRef = React.useRef<HTMLInputElement>(null)
-  React.useEffect(() => {
+  useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent): void => {
       if (e.key === 'Escape') {
         const params = new URLSearchParams(searchParams.toString())
@@ -27,20 +29,27 @@ export const ArticleSearchInput = (): React.ReactElement => {
     }
   }, [searchParams, router])
 
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      const params = new URLSearchParams()
+      params.set('keyword', inputValue)
+      router.push(`?${params.toString()}`)
+    }, 500)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [inputValue])
+
   return (
     <div className="relative w-full">
       <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-green-400"/>
       <input
         ref={inputRef}
         type="text"
-        value={keyword}
-        onInput={(e) => {
-          const inputValue = e.currentTarget.value
-          const params = new URLSearchParams()
-          if (inputValue.trim() !== '') {
-            params.set('keyword', inputValue)
-          }
-          router.push(`?${params.toString()}`)
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.currentTarget.value)
         }}
         placeholder="검색어를 입력하세요..."
         className="w-full pl-10 pr-8 p-2 bg-gray-900 text-white border border-green-400 rounded-md focus:outline-none focus:ring-2 focus:ring-green-400 transition-all duration-200 hover:border-green-300 hover:bg-gray-800"
