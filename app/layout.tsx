f23c634c51b4baaa99f1cb1b2bc8c9fd0d0fad7d
jinia91 <creee77@gmail.com>
2025-04-22 19:39:30 +0900

    ga 스크립트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { LayoutMain } from './(layout)/(components)/layout-main'
 import UiContextProvider from './(layout)/(components)/ui-context-provider'
 import { Provider } from 'jotai'
 import { SessionProvider } from './login/(components)/session-provider'
-import { Analytics } from '@vercel/analytics/next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
@@ -55,6 +55,23 @@ export default function RootLayout ({ children }: { children: React.ReactNode })
   return (
     <html lang="ko">
     <body>
+    <Script id="gtm-init" strategy="afterInteractive">
+      {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+        `}
+    </Script>
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      ></iframe>
+    </noscript>
     <Provider>
       <UiContextProvider>
         <SessionProvider>
@@ -65,7 +82,6 @@ export default function RootLayout ({ children }: { children: React.ReactNode })
         </SessionProvider>
       </UiContextProvider>
     </Provider>
-    <Analytics/>
     <SpeedInsights/>
     </body>
     </html>
